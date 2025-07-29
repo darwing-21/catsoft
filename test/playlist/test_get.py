8c2f69af9eb527a0ef73fd54aebe272f25a4def5
darwing-21 <darwingarcia2130@gmail.com>
2025-07-29 11:17:55 -0400

    feat(test): add new test case

--- test/playlist/test_get.py
+++ test/playlist/test_get.py
@@ -24,3 +24,73 @@ def test_get_categories(get_token):
     }
     response = requests.request("GET", url, headers=headers, data=payload)
     assert_status_code_200(response)
+
+
+def test_get_playlist(get_token):
+    url = f"{BASE_URI}/v1/playlists/2trIWXYIZ6NZt7Ydp1NKSN"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer ' + token
+    } 
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
+
+
+def test_get_categories(get_token):
+    url = f"{BASE_URI}/v1/browse/categories"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer ' + token
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
+    
+def test_get_playlist(get_token):
+    url = f"{BASE_URI}/v1/playlists/2trIWXYIZ6NZt7Ydp1NKSN"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer ' + token
+    } 
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
+
+
+def test_get_categories(get_token):
+    url = f"{BASE_URI}/v1/browse/categories"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer ' + token
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
+
+def test_get_playlist(get_token):
+    url = f"{BASE_URI}/v1/playlists/2trIWXYIZ6NZt7Ydp1NKSN"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer ' + token
+    } 
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
+
+
+def test_get_categories(get_token):
+    url = f"{BASE_URI}/v1/browse/categories"
+    token = get_token
+    print(token)
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer ' + token
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert_status_code_200(response)
